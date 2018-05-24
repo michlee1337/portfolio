3a2cc7d35551f56c2cae5f6b1147072cdcd6cda9
Michelle S Lee <michlee1337@gmail.com>
2018-05-24 19:15:57 +0800

    animating name using anime.js - letters M and I

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+window.onload = function(){
+  //drawing
+  var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true,
+  });
+};
